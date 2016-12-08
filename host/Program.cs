75f8db2c083e65f26032ef6d2b96ef13a9f96800
Mattish <mattishd@gmail.com>
2016-12-08 20:30:46 +0000

    file configurations

--- host/Program.cs
+++ host/Program.cs
@@ -6,12 +6,14 @@ class Program
     static void Main(string[] args)
     {
         var config = ClusterConfiguration.LocalhostPrimarySilo();
+        config.LoadFromFile("OrleansConfiguration.xml");
         config.AddMemoryStorageProvider();
         var siloHost = new Orleans.Runtime.Host.SiloHost("MyHost", config);
         siloHost.LoadOrleansConfig();
         siloHost.InitializeOrleansSilo();
         siloHost.StartOrleansSilo();
         Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
         Console.ReadKey();
         siloHost.StopOrleansSilo();
         siloHost.Dispose();
